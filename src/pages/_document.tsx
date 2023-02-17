08ae02ab1d213b496b53ae28d954494b1a39944b
RyotaAraya <58146059+RyotaAraya@users.noreply.github.com>
2023-02-17 09:53:18 +0900

    pageリファクタリング

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,10 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+JP:wght@300&display=swap"
                     rel="stylesheet"
                 />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
             </Head>
             <body>
                 <Main />
