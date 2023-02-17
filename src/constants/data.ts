08ab2e44337cf155fdde6fbb5624451427605bf4
RyotaAraya <58146059+RyotaAraya@users.noreply.github.com>
2023-02-17 12:43:23 +0900

    meal データ登録

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,12 +1,12 @@
 import { Meal } from "@/types"
 
 export const INIT_MEAL_LIST: Meal[] = [
-    { id: 0, url: "/images/meal_01.png", date: "05.21.Morning" },
-    { id: 1, url: "/images/meal_01.png", date: "05.21.Morning" },
-    { id: 2, url: "/images/meal_01.png", date: "05.21.Morning" },
-    { id: 3, url: "/images/meal_01.png", date: "05.21.Morning" },
-    { id: 4, url: "/images/meal_01.png", date: "05.21.Morning" },
-    { id: 5, url: "/images/meal_01.png", date: "05.21.Morning" },
-    { id: 6, url: "/images/meal_01.png", date: "05.21.Morning" },
-    { id: 7, url: "/images/meal_01.png", date: "05.21.Morning" },
+    { id: 0, url: "/images/meal/meal_m01.png", date: "05.21.Morning" },
+    { id: 1, url: "/images/meal/meal_l01.png", date: "05.21.Lunch" },
+    { id: 2, url: "/images/meal/meal_d01.png", date: "05.21.Dinner" },
+    { id: 3, url: "/images/meal/meal_s01.png", date: "05.21.Snack" },
+    { id: 4, url: "/images/meal/meal_m01.png", date: "05.20.Morning" },
+    { id: 5, url: "/images/meal/meal_l02.png", date: "05.20.Lunch" },
+    { id: 6, url: "/images/meal/meal_d02.png", date: "05.20.Dinner" },
+    { id: 7, url: "/images/meal/meal_s02.png", date: "05.20.Snack" },
 ]
