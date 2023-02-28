f71e5ba3cade99b22e5bf0084a6a1fde687c155a
RyotaAraya <58146059+RyotaAraya@users.noreply.github.com>
2023-02-28 15:00:36 +0900

    環境構築 login機能の確認

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import "@/styles/globals.scss"
 
 import type { AppProps } from "next/app"
-import { useContext } from "react"
+import { SessionProvider } from "next-auth/react"
 
 import Layout from "@/components/Templates/LayoutTemplate"
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <SessionProvider session={pageProps.session}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </SessionProvider>
     )
 }
