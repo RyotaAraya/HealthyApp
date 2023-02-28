2515a1a59662658cf7cdd70a49aea1ae63df7360
RyotaAraya <58146059+RyotaAraya@users.noreply.github.com>
2023-02-28 17:41:08 +0900

    graphql QueryとMutationなど作成

---
+++ src/graphql/resolvers/index.ts
@@ -0,0 +1,68 @@
+import type { Resolvers } from "@/generated/resolvers-types"
+
+export const resolve: Resolvers = {
+    Query: {
+        diaries: async (_, __, { prisma, currentUser }) => {
+            if (!currentUser) {
+                throw new Error("User not logged in.")
+            }
+            const diaries = await prisma.dairy.findMany({
+                orderBy: { createdAt: "desc" },
+                include: { user: true },
+                where: { userId: currentUser.id },
+            })
+            return diaries
+        },
+    },
+    Mutation: {
+        addDiary: async (_, { content }, { prisma, currentUser }) => {
+            if (!currentUser) {
+                throw new Error("User not logged in.")
+            }
+            const diary = await prisma.dairy.create({
+                data: { userId: currentUser.id, content },
+                include: { user: true },
+            })
+            return diary
+        },
+        updateDiary: async (
+            _,
+            { diaryId, content },
+            { prisma, currentUser }
+        ) => {
+            if (!currentUser) {
+                throw new Error("User not logged in.")
+            }
+            const targetDiary = await prisma.dairy.findUnique({
+                where: { id: diaryId },
+            })
+            if (targetDiary?.userId !== currentUser.id) {
+                throw new Error("Invalid user")
+            }
+            const diary = await prisma.dairy.update({
+                where: { id: diaryId },
+                data: {
+                    ...(content && { content }),
+                },
+                include: { user: true },
+            })
+            return diary
+        },
+        deleteDiary: async (_, { diaryId }, { prisma, currentUser }) => {
+            if (!currentUser) {
+                throw new Error("User not logged in.")
+            }
+            const targetDiary = await prisma.dairy.findUnique({
+                where: { id: diaryId },
+            })
+            if (targetDiary?.userId !== currentUser.id) {
+                throw new Error("Invalid user")
+            }
+            const diary = await prisma.dairy.delete({
+                where: { id: diaryId },
+                include: { user: true },
+            })
+            return diary
+        },
+    },
+}
