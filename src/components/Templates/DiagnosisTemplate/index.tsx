b04272c625d0d2ef9d01302ac13db3ea6c9c5ceb
RyotaAraya <58146059+RyotaAraya@users.noreply.github.com>
2023-02-28 11:34:20 +0900

    robo 準備

--- src/components/Templates/DiagnosisTemplate/index.tsx
+++ src/components/Templates/DiagnosisTemplate/index.tsx
@@ -1,7 +1,4 @@
-import TransitionMenu from "@/components/Organisms/TransitionMenu"
 import BodyRecord from "@/components/Organisms/BodyRecord"
-import MyExercise from "@/components/Organisms/MyExercise"
-import MyDiary from "@/components/Organisms/MyDiary"
 import { useDiagnosisTemplate } from "@/components/Templates/DiagnosisTemplate/useDiagnosisTemplate"
 import styles from "@/components/Templates/DiagnosisTemplate/DiagnosisTemplate.module.scss"
 import Link from "next/link"
@@ -9,6 +6,7 @@ import Link from "next/link"
 const RecordTemplate = () => {
     return (
         <>
+            <BodyRecord />
             <div className={styles.container}>
                 <h1>準備中</h1>
                 <p>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.scss"
 import type { AppProps } from "next/app"
+import { useContext } from "react"
 import Layout from "@/components/Templates/LayoutTemplate"
 
 export default function App({ Component, pageProps }: AppProps) {
