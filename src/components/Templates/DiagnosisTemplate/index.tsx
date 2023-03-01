9d6851707fb3578cf6fbcb0f3b09b31d3faee1bf
RyotaAraya <58146059+RyotaAraya@users.noreply.github.com>
2023-03-02 08:46:59 +0900

    ChatGpt土台作成

--- src/components/Templates/DiagnosisTemplate/index.tsx
+++ src/components/Templates/DiagnosisTemplate/index.tsx
@@ -1,25 +1,172 @@
-import Link from "next/link"
+"use client"
+import Image from "next/image"
+import useState from "react-usestateref"
 
 import BodyRecord from "@/components/Organisms/BodyRecord"
 import styles from "@/components/Templates/DiagnosisTemplate/DiagnosisTemplate.module.scss"
 import { useDiagnosisTemplate } from "@/components/Templates/DiagnosisTemplate/useDiagnosisTemplate"
 
-const RecordTemplate = () => {
+enum Creator {
+    Me = 0,
+    Bot = 1,
+}
+
+interface MessageProps {
+    text: string
+    from: Creator
+    key: number
+}
+
+interface InputProps {
+    onSend: (input: string) => void
+    disabled: boolean
+}
+
+const ChatMessage = ({ text, from }: MessageProps) => {
     return (
         <>
-            <BodyRecord />
-            <div className={styles.container}>
-                <h1>準備中</h1>
-                <p>
-                    体重と身長、持病情報などを用いてChatGPTにアドバイスを求める。
-                </p>
-                <p>以前作成したAIbotを応用予定</p>
-                <Link href={"https://phone-ten.vercel.app/auth/chat-gpt"}>
-                    https://phone-ten.vercel.app/auth/chat-gpt
-                </Link>
-            </div>
+            {from == Creator.Me && (
+                <div className="bg-white p-r rounded-lg flex gap-4 items-center whitespace-pre-wrap">
+                    <Image
+                        src={"/icons/icon_user.svg"}
+                        alt="User"
+                        width={40}
+                        height={40}
+                    />
+                    <p className="text-gray-700">{text}</p>
+                </div>
+            )}
+            {from == Creator.Bot && (
+                <div className="bg-gray-100 p-4 rounded-lg flex gap-4 items-center whitespace-pre-wrap">
+                    <Image
+                        src={"/icons/icon_bot.svg"}
+                        alt="Bot"
+                        width={40}
+                        height={40}
+                    />
+                    <p className="text-gray-700">{text}</p>
+                </div>
+            )}
         </>
     )
 }
 
-export default RecordTemplate
+export const DiagnosisTemplae = () => {
+    const [input, setInput] = useState("")
+    const [robRes, setRobRes, setRobRef] = useState("")
+    const [message, setMessages, messagesRef] = useState<MessageProps[]>([])
+    const [loading, setLoading] = useState(false)
+
+    const callApi = async () => {
+        setLoading(true)
+
+        const myMessage: MessageProps = {
+            text: input,
+            from: Creator.Me,
+            key: new Date().getTime(),
+        }
+
+        setMessages([myMessage, ...messagesRef.current])
+
+        try {
+            const response = await fetch("/api/generate-answer", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    prompt: input,
+                }),
+            })
+
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+
+            const data = response.body
+            if (!data) {
+                return
+            }
+            const reader = data.getReader()
+            const decoder = new TextDecoder()
+            let done = false
+
+            let tmp = ""
+            while (!done) {
+                const { value, done: doneReading } = await reader.read()
+                done = doneReading
+                const chunkValue = decoder.decode(value)
+                tmp = tmp + chunkValue
+            }
+            setRobRes(tmp)
+
+            const botMessage: MessageProps = {
+                text: setRobRef.current,
+                from: Creator.Bot,
+                key: new Date().getTime(),
+            }
+            setLoading(false)
+            setMessages([botMessage, ...messagesRef.current])
+        } catch (e) {
+            alert(e)
+        }
+    }
+    const handleSubmit = () => {
+        if (input === "") return
+        callApi()
+        setInput("")
+    }
+
+    return (
+        <main className="relative max-w-2x mx-auto">
+            <div className="sticky top-0 w-full pt-10 px-4">
+                <div className="bg-white border-2 p-2 rounded-lg flex justify-center">
+                    <input
+                        value={input}
+                        onChange={(e: any) => setInput(e.target.value)}
+                        className="w-full py-2 px-3 text-gray-800 rounded-lg focus:outline-none"
+                        type="text"
+                        placeholder="質問をどうぞ"
+                        disabled={loading}
+                    />
+                    {!loading && message.length < 4 && (
+                        <button
+                            onClick={() => handleSubmit()}
+                            className="p-2 bg-gray-200 rounded-md text-gray-500 bottom-1.5 righit-1"
+                        >
+                            Ask
+                        </button>
+                    )}
+                </div>
+                <div className="top-4">
+                    {loading && (
+                        <p className="text-gray-500 flex justify-center">
+                            時間がかかる場合があります。1分ほど待っても取得できない場合は再度お試しください。
+                        </p>
+                    )}
+                    {loading && <p>Loading...</p>}
+                </div>
+            </div>
+
+            <div className="mt-10 px-4">
+                {message.map((msg: MessageProps) => (
+                    <ChatMessage
+                        key={msg.key}
+                        text={msg.text}
+                        from={msg.from}
+                    />
+                ))}
+                {message.length === 0 && (
+                    <p className="text-center text-gray-400">
+                        2回質問できます。
+                    </p>
+                )}
+                {message.length >= 4 && (
+                    <p className="text-center text-red-400">終了です。</p>
+                )}
+            </div>
+        </main>
+    )
+}
+
+export default DiagnosisTemplae
