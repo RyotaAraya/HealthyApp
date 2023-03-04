2ed8298292d4e40dd480607a53d453cbfefb3261
RyotaAraya <58146059+RyotaAraya@users.noreply.github.com>
2023-03-04 12:03:55 +0900

    jest react-testing-libray実装

---
+++ src/components/Atoms/Button/index.test.tsx
@@ -0,0 +1,34 @@
+import "@testing-library/jest-dom"
+
+import { fireEvent, render, screen } from "@testing-library/react"
+
+import Button from "@/components/Atoms/Button"
+
+describe("Button", () => {
+    it("renders with the correct title", () => {
+        const title = "Click me!"
+        const { getByText } = render(
+            <Button title={title} handlePushButton={() => {}} />
+        )
+        const button = getByText(title)
+        expect(button).toBeInTheDocument()
+    })
+
+    it("calls the onClick callback when clicked", () => {
+        const handlePushButton = jest.fn()
+        const { getByRole } = render(
+            <Button title="Click me!" handlePushButton={handlePushButton} />
+        )
+        const button = getByRole("button")
+        fireEvent.click(button)
+        expect(handlePushButton).toHaveBeenCalledTimes(1)
+    })
+
+    it("applies the correct CSS class", () => {
+        const { getByRole } = render(
+            <Button title="Click me!" handlePushButton={() => {}} />
+        )
+        const button = getByRole("button")
+        expect(button).toHaveClass("container")
+    })
+})
